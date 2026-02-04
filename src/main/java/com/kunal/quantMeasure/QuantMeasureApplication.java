c68a84ed408c529289ee298a8da0734edf1910df
Kunal Dubey <ku20646303@wipro.com>
2026-02-04 16:03:58 +0530

    Initial Project Commit

---
+++ src/main/java/com/kunal/quantMeasure/QuantMeasureApplication.java
@@ -0,0 +1,13 @@
+package com.kunal.quantMeasure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantMeasureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantMeasureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kunal/quantMeasure/QuantMeasureApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kunal.quantMeasure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantMeasureApplicationTests {
+
+	@Test
+	void contextLoad(){
+
+	}
+
+}
